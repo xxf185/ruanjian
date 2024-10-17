01d08fc61649d0a1596aa5bdbb62fd1a435cd7bb
xxf185 <109097973+xxf185@users.noreply.github.com>
2024-10-17 23:43:28 +0800

    Create djxs.js

---
+++ djxs.js
@@ -0,0 +1,9 @@
+#!name=得间小说
+#!desc=得间小说解锁
+  
+[Script]
+djxs=type=http-response,pattern=^http[s]?:\/\/dj.palmestore.com\/zyuc\/api\/user\/accountInfo,requires-body=1,script-path=https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/djxs.js
+
+
+[MITM]
+hostname = %APPEND% dj.palmestore.com
